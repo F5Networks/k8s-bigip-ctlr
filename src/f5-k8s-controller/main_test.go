b1c3ea1f1e8a47f4aed247b4f36f784d2b4d9589
Matthew Yacobucci <m.yacobucci@f5.com>
2016-09-29 19:46:50 -0600

    VEL-373 Add unit test for controller sub process utility
    Problem: The controller sub process running the pythong bigip config script
    needs unit testing.
    
    Solution: Refactored the sub process routines to enable unit testing, pass
    in more configuration to a cmd create function and the run function
    
    Add unit tests

---
+++ src/f5-k8s-controller/main_test.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"os"
+	"os/exec"
+	"testing"
+	"time"
+)
+
+func TestDriverCmd(t *testing.T) {
+	username := "admin"
+	password := "test"
+	partition := "velcro"
+	url := "bigip.example.com"
+	pyDriver := "/tmp/some-dir/test-driver.py"
+
+	configFile := fmt.Sprintf("/tmp/f5-k8s-controller.config.%d.json",
+		os.Getpid())
+
+	pythonPath, err := exec.LookPath("python")
+	assert.Nil(t, err, "We should find python")
+
+	cmd := createDriverCmd(username, password, url, partition, pyDriver)
+
+	require.NotNil(t, cmd, "Command should not be nil")
+	require.NotNil(t, cmd.Path, "Path should not be nil")
+	require.Equal(t, cmd.Path, pythonPath, "The command path should be python")
+
+	args := []string{
+		"python",
+		pyDriver,
+		"--username", username,
+		"--password", password,
+		"--hostname", url,
+		"--config-file", configFile,
+		partition}
+	require.EqualValues(t, cmd.Args, args, "We should get expected args list")
+}
+
+func TestDriverSubProcess(t *testing.T) {
+	subPidCh := make(chan int)
+
+	username := "admin"
+	password := "test"
+	partition := "velcro"
+	url := "bigip.example.com"
+	pyDriver := "./test/pyTest.py"
+
+	cmd := createDriverCmd(username, password, url, partition, pyDriver)
+	go runBigIpDriver(subPidCh, cmd)
+	pid := <-subPidCh
+
+	time.Sleep(time.Second)
+
+	assert.NotEqual(t, pid, 0, "Pid should be set and not nil value")
+
+	proc, err := os.FindProcess(pid)
+	assert.NoError(t, err)
+	assert.NotNil(t, proc, "Should have process object")
+
+	done := make(chan error)
+
+	go func() {
+		count := 0
+		var testErr error = nil
+	forever:
+		for {
+			count += 1
+			cmd := exec.Command("bash", []string{"test/testPyTest.sh"}...)
+			err := cmd.Start()
+			if err != nil {
+				testErr = errors.New("Should not error starting bash command")
+				break forever
+			}
+			err = cmd.Wait()
+			if _, ok := err.(*exec.ExitError); ok {
+				break forever
+			}
+
+			if count == 30 {
+				testErr = errors.New("Timed out waiting for process to stop")
+				break forever
+			}
+
+			<-time.After(time.Second)
+		}
+
+		done <- testErr
+	}()
+
+	err = proc.Signal(os.Interrupt)
+	require.NoError(t, err)
+
+	err = <-done
+	require.NoError(t, err)
+}
---
+++ src/f5-k8s-controller/test/testPyTest.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ps -ef | grep -v grep | grep "test/pyTest.py"

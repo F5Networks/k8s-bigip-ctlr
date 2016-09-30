94a9276faf9592dd131ba4da6dd233179c26bd55
Matthew Yacobucci <m.yacobucci@f5.com>
2016-09-30 13:47:00 -0600

    VEL-373 virtualServer.getNodeAddresses unit test
    Problem: virtualServer.getNodeAddresses needs unit testing
    
    Solution: added unit test

--- src/virtualServer/virtualServer_test.go
+++ src/virtualServer/virtualServer_test.go
@@ -1,10 +1,16 @@
 package virtualServer
 
 import (
-	"github.com/stretchr/testify/assert"
 	"os"
 	"strconv"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"k8s.io/client-go/1.4/kubernetes/fake"
+	"k8s.io/client-go/1.4/pkg/api"
+	"k8s.io/client-go/1.4/pkg/api/unversioned"
+	"k8s.io/client-go/1.4/pkg/api/v1"
 )
 
 func TestConfigFilename(t *testing.T) {
@@ -16,4 +22,70 @@ func TestConfigFilename(t *testing.T) {
 	assert.Equal(expectedFilename, OutputFilename)
 }
 
-// FIXME: Add more unit tests
+func newNode(id, rv string, unsched bool,
+	addresses []v1.NodeAddress) *v1.Node {
+	return &v1.Node{
+		TypeMeta: unversioned.TypeMeta{
+			Kind:       "Node",
+			APIVersion: "v1",
+		},
+		ObjectMeta: v1.ObjectMeta{
+			Name:            id,
+			ResourceVersion: rv,
+		},
+		Spec: v1.NodeSpec{
+			Unschedulable: unsched,
+		},
+		Status: v1.NodeStatus{
+			Addresses: addresses,
+		},
+	}
+}
+
+func TestGetAddresses(t *testing.T) {
+	// Existing Node data
+	expectedNodes := []*v1.Node{
+		newNode("node0", "0", true, []v1.NodeAddress{
+			{"ExternalIP", "127.0.0.0"}}),
+		newNode("node1", "1", false, []v1.NodeAddress{
+			{"ExternalIP", "127.0.0.1"}}),
+		newNode("node2", "2", false, []v1.NodeAddress{
+			{"ExternalIP", "127.0.0.2"}}),
+		newNode("node3", "3", false, []v1.NodeAddress{
+			{"ExternalIP", "127.0.0.3"}}),
+		newNode("node4", "4", false, []v1.NodeAddress{
+			{"InternalIP", "127.0.0.4"}}),
+		newNode("node5", "5", false, []v1.NodeAddress{
+			{"Hostname", "127.0.0.5"}}),
+	}
+
+	expectedReturn := []string{
+		"127.0.0.1",
+		"127.0.0.2",
+		"127.0.0.3",
+	}
+
+	fake := fake.NewSimpleClientset()
+
+	for _, expectedNode := range expectedNodes {
+		node, err := fake.Core().Nodes().Create(expectedNode)
+		require.Nil(t, err, "Should not fail creating node")
+		require.EqualValues(t, expectedNode, node, "Nodes should be equal")
+	}
+
+	addresses, err := getNodeAddresses(fake)
+	require.Nil(t, err, "Should not fail getting addresses")
+	assert.EqualValues(t, expectedReturn, addresses,
+		"Should receive the correct addresses")
+
+	for _, node := range expectedNodes {
+		err := fake.Core().Nodes().Delete(node.ObjectMeta.Name,
+			&api.DeleteOptions{})
+		require.Nil(t, err, "Should not fail deleting node")
+	}
+
+	expectedReturn = []string{}
+	addresses, err = getNodeAddresses(fake)
+	require.Nil(t, err, "Should not fail getting empty addresses")
+	assert.EqualValues(t, expectedReturn, addresses, "Should get no addresses")
+}

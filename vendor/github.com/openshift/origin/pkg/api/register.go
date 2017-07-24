2aed195459429148105d41df446a9ba3995a13a1
Gary Ritzer <garyr@f5.com>
2017-07-24 09:25:49 -0600

    Add OpenShift dependencies to add support for Route objects
    Problem:
    The controller project needs to include its openshift dependencies so
    Route objects can be supported in OpenShift.
    
    Solution:
    Added the necessary dependencies to support OpenShift route objects.
    Updated the .f5license file to contain the correct entries for
    attributions generation.
    
    affects-branches: master

---
+++ vendor/github.com/openshift/origin/pkg/api/register.go
@@ -0,0 +1,37 @@
+package api
+
+import (
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+
+	_ "github.com/openshift/origin/pkg/authorization/api"
+	_ "github.com/openshift/origin/pkg/build/api"
+	_ "github.com/openshift/origin/pkg/deploy/api"
+	_ "github.com/openshift/origin/pkg/image/api"
+	_ "github.com/openshift/origin/pkg/oauth/api"
+	_ "github.com/openshift/origin/pkg/project/api"
+	_ "github.com/openshift/origin/pkg/route/api"
+	_ "github.com/openshift/origin/pkg/sdn/api"
+	_ "github.com/openshift/origin/pkg/security/api"
+	_ "github.com/openshift/origin/pkg/template/api"
+	_ "github.com/openshift/origin/pkg/user/api"
+)
+
+const (
+	Prefix      = "/oapi"
+	GroupPrefix = "/apis"
+	GroupName   = ""
+)
+
+// SchemeGroupVersion is group version used to register these objects
+var SchemeGroupVersion = schema.GroupVersion{Group: GroupName, Version: runtime.APIVersionInternal}
+
+// Kind takes an unqualified kind and returns back a Group qualified GroupKind
+func Kind(kind string) schema.GroupKind {
+	return SchemeGroupVersion.WithKind(kind).GroupKind()
+}
+
+// Resource takes an unqualified resource and returns back a Group qualified GroupResource
+func Resource(resource string) schema.GroupResource {
+	return SchemeGroupVersion.WithResource(resource).GroupResource()
+}
---
+++ vendor/github.com/openshift/origin/pkg/route/api/v1/defaults.go
@@ -0,0 +1,53 @@
+package v1
+
+import "k8s.io/apimachinery/pkg/runtime"
+
+// If adding or changing route defaults, updates may be required to
+// pkg/router/controller/controller.go to ensure the routes generated from
+// ingress resources will match routes created via the api.
+
+func SetDefaults_RouteSpec(obj *RouteSpec) {
+	if len(obj.WildcardPolicy) == 0 {
+		obj.WildcardPolicy = WildcardPolicyNone
+	}
+}
+
+func SetDefaults_RouteTargetReference(obj *RouteTargetReference) {
+	if len(obj.Kind) == 0 {
+		obj.Kind = "Service"
+	}
+	if obj.Weight == nil {
+		obj.Weight = new(int32)
+		*obj.Weight = 100
+	}
+}
+
+func SetDefaults_TLSConfig(obj *TLSConfig) {
+	if len(obj.Termination) == 0 && len(obj.DestinationCACertificate) == 0 {
+		obj.Termination = TLSTerminationEdge
+	}
+	switch obj.Termination {
+	case TLSTerminationType("Reencrypt"):
+		obj.Termination = TLSTerminationReencrypt
+	case TLSTerminationType("Edge"):
+		obj.Termination = TLSTerminationEdge
+	case TLSTerminationType("Passthrough"):
+		obj.Termination = TLSTerminationPassthrough
+	}
+}
+
+func SetDefaults_RouteIngress(obj *RouteIngress) {
+	if len(obj.WildcardPolicy) == 0 {
+		obj.WildcardPolicy = WildcardPolicyNone
+	}
+}
+
+func addDefaultingFuncs(scheme *runtime.Scheme) error {
+	RegisterDefaults(scheme)
+	return scheme.AddDefaultingFuncs(
+		SetDefaults_RouteSpec,
+		SetDefaults_RouteTargetReference,
+		SetDefaults_TLSConfig,
+		SetDefaults_RouteIngress,
+	)
+}

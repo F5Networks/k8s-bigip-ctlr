6fcb4f1d3f393003a5e8969e9307e4c6160c1917
Saylor Berman <s.berman@f5.com>
2017-09-27 19:27:52 +0000

    Improve travis build times
    Problem: The travis build was taking a long time due to the devel containers
    being built more times than necessary.
    
    Solution: Remove unnecessary devel builds. Also specified the number of compilers
    used in the Ginkgo build. This *may* help with ginkgo run times, though
    that has yet to be seen.

--- build-tools/build-debug-artifacts.sh
+++ build-tools/build-debug-artifacts.sh
@@ -9,16 +9,7 @@ CURDIR="$(dirname $BASH_SOURCE)"
 
 . $CURDIR/_build-lib.sh
 
-# Build the builder image.
-$CURDIR/build-devel-image.sh
-
 # Build artifacts using the build image
 export build_img=$BUILD_DBG_IMG_TAG
+$CURDIR/run-in-docker.sh make verify
 $CURDIR/run-in-docker.sh ./build-tools/dbg-build.sh
-
-if $CLEAN_BUILD; then
-  docker rmi $build_img
-fi
-
-# Now ready to run ./build-debug-images.sh
-

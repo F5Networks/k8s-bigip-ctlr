682cb828f897cedb5892430561f8b968922e0442
Ashok Mudukutore <a.mudukutore@f5.com>
2018-03-05 16:58:53 -0700

    Update docs version on version-tag commits
    Problem:
    Docs version string not being updated when a new version
    tag is pushed. Also, docs are not published when new version
    tags are pushed
    
    Solution:
    1. Changed the criteria for updating docs version string to include
    version-tag expected string.
    2. Changed condition in travis which allows docs publish
    when new version tags are pushed.

--- build-tools/docker-docs.sh
+++ build-tools/docker-docs.sh
@@ -13,7 +13,7 @@ RUN_ARGS=( \
   -e TRAVIS=$TRAVIS
 )
 
-if [[ $TRAVIS_BRANCH == *"-stable" ]]; then
+if [[ $TRAVIS_BRANCH == *"-stable" || "$TRAVIS_BRANCH" =~ ^v[0-9]+\.[0-9]+\.[0-9]* ]]; then
   release="$(git describe --tags --abbrev=0)"
   RUN_ARGS+=( -e DOCS_RELEASE=$release )
   va=( ${release//./ } ) # replace decimals and split into array

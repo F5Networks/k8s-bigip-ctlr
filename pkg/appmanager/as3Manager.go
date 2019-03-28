6c31d254b7474267be7fd3101c76733b187645d3
Trinath Somanchi <trinaths.dev@gmail.com>
2019-03-28 18:31:43 +0530

    Handle AS3 Templates with no ADC class.
    Problem:
    Without ADC class also, AS3 Template is being parsed and Controller is crashing.
    
    Solution:
    Updated logic not to handle any AS3 Template without ADC class.
    
    All related unit tests pass.
    
    affects-branches: feature.user-defined-as3

--- pkg/appmanager/as3Manager.go
+++ pkg/appmanager/as3Manager.go
@@ -1,3 +1,19 @@
+/*-
+ * Copyright (c) 2016-2018, F5 Networks, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package appmanager
 
 import (
@@ -67,6 +83,10 @@ func (appMgr *Manager) getAS3ObjectFromTemplate(
 	as3 := make(as3Object)
 	// extract as3 declaration from template
 	dclr := (tmpl.(map[string]interface{}))["declaration"]
+	if dclr == nil {
+		log.Error("No ADC class declaration found.")
+		return nil, false
+	}
 
 	// Loop over all the tenants
 	for tn, t := range dclr.(map[string]interface{}) {

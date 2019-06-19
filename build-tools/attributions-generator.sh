031b7aea890cf700efc05751c02e02aa25fcba5a
vklohiya <vklohiya@live.com>
2019-06-19 13:02:05 +0530

    Created the Gitlab CI.

--- build-tools/attributions-generator.sh
+++ build-tools/attributions-generator.sh
@@ -10,10 +10,15 @@ set -x
 ATTR_GEN_IMG=f5networksdevel/attributions-generator:latest
 docker pull ${ATTR_GEN_IMG}
 
+LOCAL_USER_ID=$(id -u)
+if [ "$GITLAB_CI" == true ]; then
+  LOCAL_USER_ID=9001
+fi
+
 RUN_ARGS=( \
   --rm
   -v $PWD:$PWD
-  -e LOCAL_USER_ID=$(id -u)
+  -e LOCAL_USER_ID=$LOCAL_USER_ID
 )
 
 docker run "${RUN_ARGS[@]}" ${ATTR_GEN_IMG}  "$@"
--- build-tools/run-in-docker.sh
+++ build-tools/run-in-docker.sh
@@ -16,22 +16,28 @@ CURDIR="$(dirname $BASH_SOURCE)"
 : ${build_img:=${BUILD_IMG_TAG}}
 
 # Need to make the directory before docker, to keep it owned by local user
-srcdir=src/github.com/F5Networks/k8s-bigip-ctlr
-wkspace=${PWD}/_docker_workspace
-mkdir -p $wkspace/$srcdir
-
+srcdir=/build/src/github.com/F5Networks/k8s-bigip-ctlr/
+#wkspace=${PWD}/_docker_workspace
+#mkdir -p $wkspace/$srcdir
+#LOCAL_USER_ID=$(id -u)
+LOCAL_USER_ID=9001
+if [ "$GITLAB_CI" == true ]; then
+  TRAVIS_REPO_SLUG=$CI_PROJECT_PATH
+  LOCAL_USER_ID=9001
+fi
 RUN_ARGS=( \
   --rm
-  -v $wkspace:/build:Z
-  -v $PWD:/build/$srcdir:ro,Z
-  --workdir  /build/$srcdir
+#  -v $wkspace:/build:Z
+#  -v $PWD:/build/$srcdir:ro,Z
+  -v workspace_vol:/build/
+  --workdir  $srcdir
   -e GOPATH=/build
   -e CLEAN_BUILD=$CLEAN_BUILD
   -e IMG_TAG=$IMG_TAG
   -e BUILD_IMG_TAG=$BUILD_IMG_TAG
   -e BUILD_VERSION=$BUILD_VERSION
   -e BUILD_INFO=$BUILD_INFO
-  -e LOCAL_USER_ID=$(id -u)
+  -e LOCAL_USER_ID=$LOCAL_USER_ID
   -e TRAVIS_REPO_SLUG=$TRAVIS_REPO_SLUG
   -e COVERALLS_TOKEN=$COVERALLS_REPO_TOKEN
   -e RUN_TESTS=$RUN_TESTS
@@ -42,6 +48,5 @@ RUN_ARGS=( \
 if [ -t 0 ]; then
   RUN_ARGS+=( "-it" )
 fi
-
 # Run the user provided args
 docker run "${RUN_ARGS[@]}" "$build_img" "$@"

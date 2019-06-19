6af33871b45abff33da3ee798ff63f7e1e5219b2
vklohiya <vklohiya@live.com>
2019-06-20 01:27:30 +0530

    Update make-docs.sh
    Adding gitlab ci support

--- build-tools/make-docs.sh
+++ build-tools/make-docs.sh
@@ -12,7 +12,7 @@ write-good `find docs -name '*.rst'` --passive --so --no-illusion --thereIs --cl
 
 # Exclude release notes from linkcheck when running in travis.
 # Note: we've already rendered these - its safe to remove the unneeded source files.
-if [ "$TRAVIS" = true ]; then
+if [ "$TRAVIS" == true or CI_SERVER_NAME == "GitLab" ]; then
   rm -f docs/RELEASE-NOTES.rst
 fi
 make -C docs linkcheck

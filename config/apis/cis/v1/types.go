e8389ad8d32dc783fecd2833befdce1afffe75b0
Abhishek VEERAMALLA <a.veeramalla@f5.com>
2020-03-19 23:35:49 +0530

    Add Informer support for CRD

---
+++ config/apis/cis/v1/types.go
@@ -0,0 +1,39 @@
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +kubebuilder:validation:Optional
+
+// VirtualServer defines the VirtualServer resource.
+type VirtualServer struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec VirtualServerSpec `json:"spec"`
+}
+
+// VirtualServerSpec is the spec of the VirtualServer resource.
+type VirtualServerSpec struct {
+	Host  string `json:"host"`
+	Pools []Pool `json:"pools"`
+}
+
+// Pool defines a pool object in BIG-IP.
+type Pool struct {
+	Path    string `json:"path"`
+	Service string `json:"service"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// VirtualServerList is a list of the VirtualServer resources.
+type VirtualServerList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
+
+	Items []VirtualServer `json:"items"`
+}

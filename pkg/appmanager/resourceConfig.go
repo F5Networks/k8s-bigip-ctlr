40eb1578941815d96c7dc53ac1ea806f56589860
Amit Gupta <amitg2812@gmail.com>
2020-06-24 09:55:52 +0530

    Whitelist feature support with agent AS3 added using policy endpoint condition

--- pkg/appmanager/resourceConfig.go
+++ pkg/appmanager/resourceConfig.go
@@ -91,8 +91,11 @@ func (appMgr *Manager) createRSConfigFromIngress(
 	}
 
 	// Handle whitelist-source-range annotation
+	// Handle allow-source-range annotation
 	var whitelistSourceRanges []string
 	if sourceRange, ok := ing.ObjectMeta.Annotations[F5VsWhitelistSourceRangeAnnotation]; ok {
+		whitelistSourceRanges = ParseWhitelistSourceRangeAnnotations(sourceRange)
+	} else if sourceRange, ok := ing.ObjectMeta.Annotations[F5VsAllowSourceRangeAnnotation]; ok {
 		whitelistSourceRanges = ParseWhitelistSourceRangeAnnotations(sourceRange)
 	}
 
@@ -619,9 +622,11 @@ func (appMgr *Manager) handleRouteRules(
 		rc.MergeRules(appMgr.mergedRulesMap)
 	}
 
-	// Add whitelist condition
+	// Add whitelist or allow source condition
 	var whitelistSourceRanges []string
 	if sourceRange, ok := route.ObjectMeta.Annotations[F5VsWhitelistSourceRangeAnnotation]; ok {
+		whitelistSourceRanges = ParseWhitelistSourceRangeAnnotations(sourceRange)
+	} else if sourceRange, ok := route.ObjectMeta.Annotations[F5VsAllowSourceRangeAnnotation]; ok {
 		whitelistSourceRanges = ParseWhitelistSourceRangeAnnotations(sourceRange)
 	}
 	if len(whitelistSourceRanges) > 0 {

d6f902d48ea5fb0a5632fc311308a1f7d38c965e
“Lavanya <l.sirigudi@f5.com>
2021-01-29 22:18:46 +0530

    Adding azure pipeline and PR template

--- build-tools/rel-build.sh
+++ build-tools/rel-build.sh
@@ -11,12 +11,13 @@ DEBUG=${DEBUG:-1}
 BUILDDIR=$(get_builddir)
 export BUILDDIR=$BUILDDIR
 
-# Licensee need this path to generate attributions
-vendor_dir="$CURDIR/../../k8s-bigip-ctlr/vendor"
-. $CURDIR/attributions-generator.sh
-# Run the attributions and save the content to a local file.
-generate_attributions_licensee $vendor_dir >> /build/all_attributions.txt
-
+if $LICENSE; then
+  # Licensee need this path to generate attributions
+  vendor_dir="$CURDIR/../../k8s-bigip-ctlr/vendor"
+  . $CURDIR/attributions-generator.sh
+  # Run the attributions and save the content to a local file.
+  generate_attributions_licensee $vendor_dir >> /tmp/all_attributions.txt
+fi
 DEBUG=$DEBUG go_install $(all_cmds)
 
 if [ $RUN_TESTS -eq 1 ]; then
@@ -33,8 +34,10 @@ if [ $RUN_TESTS -eq 1 ]; then
       cat $BUILDDIR/coverage/coverage.out >> $BUILDDIR/coverage.out
       goveralls \
         -coverprofile=$BUILDDIR/coverage.out \
-        -service=travis-ci
+        -service=azure
     fi
 fi
+
+mkdir -p /build/mnt
 #Copying the build directory to volume
 cp -rf $BUILDDIR /build/mnt/

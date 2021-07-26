2600434309467dd2df7657add6f36850605826c7
Sravya <46847681+sravyap135@users.noreply.github.com>
2021-07-26 18:54:29 +0530

    Using Golang as base image for multistage dockerfile (#1828)

--- build-tools/_build-lib.sh
+++ build-tools/_build-lib.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-# This is the common enviromnent for building the runtime image, and the
+# This is the common environment for building the runtime image, and the
 # reusable developer image
 #
 # The build uses two docker container types:
@@ -16,16 +16,6 @@
 
 set -e
 
-# CI Should set these variables
-: ${CLEAN_BUILD:=false}
-: ${IMG_TAG:=k8s-bigip-ctlr:latest}
-: ${BUILD_IMG_TAG:=k8s-bigip-ctlr-devel:latest}
-: ${BUILD_VARIANT:=release}
-: ${BUILD_VARIANT_FLAGS:=}
-
-PKGIMPORT="github.com/F5Networks/k8s-bigip-ctlr"
-
-
 if [[ $BUILD_VERSION == "" ]]; then
   echo "Must set BUILD_VERSION"
   false
@@ -35,124 +25,24 @@ if [[ $BUILD_INFO == "" ]]; then
   false
 fi
 
-
-# Defer calculating build dir until actualy in the build environment
-get_builddir() {
-# Ensure PWD starts with GOPATH
-  if [ "${PWD##$GOPATH}" == "${PWD}" ]; then
-    echo '$PWD is not in $GOPATH. Refusing to continue.'
-    exit 1
-  fi
-
-  local platform="$(go env GOHOSTOS)-$(go env GOHOSTARCH)-${BUILD_VARIANT}"
-  local govers=$(go version  | awk '{print $3}')
-
-  echo "${GOPATH}/out/$platform-$govers"
-}
-
-# This is the expected output location, from the release build container
-RELEASE_PLATFORM=linux-amd64-release-go1.15
-
-NO_CACHE_ARGS=""
-if $CLEAN_BUILD; then
-  NO_CACHE_ARGS="--no-cache"
-fi
-
-
-echodo() {
-  printf " + %s\n" "$*" >&2
-  "$@"
-}
-
-
-#TODO: Should GOBIN be set too?
-go_install () {
-  # Declare seperate from assign, so failures aren't maked by local
-  local BUILDDIR
-  BUILDDIR=$(get_builddir)
-  local GO_BUILD_FLAGS=( -v -ldflags "-extldflags \"-static\" -X main.version=${BUILD_VERSION} -X main.buildInfo=${BUILD_INFO}" )
-
-  if [ $DEBUG == 0 ]
-  then
-    GO_BUILD_FLAGS+=( -gcflags "all=-N -l" )
-  fi
-
-  mkdir -p "$BUILDDIR"
-  (
-    export GOBIN="$BUILDDIR/bin"
-    echodo cd "$BUILDDIR"
-    echodo go install $BUILD_VARIANT_FLAGS "${GO_BUILD_FLAGS[@]}" -v "$@"
-  )
-}
-
 ginkgo_test_with_coverage () {
-  local WKDIR=$(tmpdir_for_test)
-  BUILDDIR=$(get_builddir)
-  # Set our gopath to the tmp dir for package import resolution
-  export GOPATH=$WKDIR
-
-  (
-    export GOBIN="$BUILDDIR/bin"
-    cd "$WKDIR/src/$PKGIMPORT"
     ginkgo -r -compilers 1 -keepGoing -trace -randomizeAllSpecs -progress --nodes 4 -cover
     echo "Gathering unit test code coverage for 'release' build..."
     gather_coverage $WKDIR
-    chmod 755 -R $WKDIR
-    rm -rf $WKDIR
-    export GOPATH=/build
-  )
 }
 
 ginkgo_test_with_profile () {
-  local WKDIR=$(tmpdir_for_test)
-  BUILDDIR=$(get_builddir)
-  mkdir -p $BUILDDIR/profile
-  export GOPATH=$WKDIR
-  (
-    export GOBIN="$BUILDDIR/bin"
-    cd "$WKDIR/src/$PKGIMPORT"
     ginkgo -r -compilers 1 -keepGoing -randomizeAllSpecs -progress --nodes 4 \
             ${BUILD_VARIANT_FLAGS} -- \
             -test.cpuprofile profile.cpu \
             -test.blockprofile profile.block \
             -test.memprofile profile.mem
-    rsync -a -f"+ */" -f"+ profile.cpu" -f"+ profile.block" -f"+ profile.mem" -f"- *" . $BUILDDIR/profile
-    rm -rf $WKDIR
-    export GOPATH=/build
-  )
-}
-
-all_cmds() {
-  echodo go list ./cmd/...
-}
-all_pkgs() {
-  echodo go list ./pkg/...
 }
 
 gather_coverage() {
-	local WKDIR="$1"
-	mkdir -p $BUILDDIR/coverage
-
-  (
-    cd $WKDIR/src/github.com/F5Networks
     gocovmerge `find . -name *.coverprofile` > coverage.out
     go tool cover -html=coverage.out -o coverage.html
     go tool cover -func=coverage.out
     # Total coverage for CI
     go tool cover -func=coverage.out | grep "^total:" | awk 'END { print "Total coverage:", $3, "of statements" }'
-    rsync -a -f"+ */" -f"+ *.coverprofile" -f"+ coverage.html" -f"+ coverage.out" -f"- *" . $BUILDDIR/coverage
-  )
-}
-
-# Create a tmp dir with go src files in a writable location
-tmpdir_for_test() {
-  BUILDDIR=$(get_builddir)
-  mkdir -p $BUILDDIR
-  # Create a temp dir we can write to
-  local WKDIR=$(mktemp -d $BUILDDIR/tmpXXXXXX)
-  # src dir to follow gopath convention
-  mkdir -p $WKDIR/src
-  # Copy over mounted src to our writable src
-  rsync -a --exclude '.git' --exclude '_docker_workspace' $GOPATH/src/ $WKDIR/src
-  echo $WKDIR
 }
--- build-tools/rel-build.sh
+++ build-tools/rel-build.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-
 set -ex
 
 CURDIR="$(dirname $BASH_SOURCE)"
 RUN_TESTS=${RUN_TESTS:-1}
-DEBUG=${DEBUG:-1}
 LICENSE=${LICENSE:-0}
 
 . $CURDIR/_build-lib.sh
-BUILDDIR=$(get_builddir)
-export BUILDDIR=$BUILDDIR
 
 if [ $LICENSE == 1 ]; then
   # Licensee need this path to generate attributions
@@ -20,26 +16,16 @@ if [ $LICENSE == 1 ]; then
   generate_attributions_licensee $vendor_dir > /build/all_attributions.txt
 fi
 
-DEBUG=$DEBUG go_install $(all_cmds)
-
 if [ $RUN_TESTS -eq 1 ]; then
     echo "Gathering unit test code coverage for 'release' build..."
     ginkgo_test_with_coverage
 fi
 
-# reset GOPATH after using temp directories
-export GOPATH=/build
-
 if [ $RUN_TESTS -eq 1 ]; then
     # push coverage data to coveralls if F5 repo or if configured for fork.
     if [ "$COVERALLS_TOKEN" ]; then
-      cat $BUILDDIR/coverage/coverage.out >> $BUILDDIR/coverage.out
       goveralls \
-        -coverprofile=$BUILDDIR/coverage.out \
+        -coverprofile=./coverage/coverage.out \
         -service=azure
     fi
 fi
-
-mkdir -p /build/mnt
-#Copying the build directory to volume
-cp -rf $BUILDDIR /build/mnt/

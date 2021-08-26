3a1933be4c1066c32bdb4aba652b594f8cc839d2
Vivek Lohiya <vklohiya@live.com>
2021-08-26 11:17:31 +0530

    Fix for vendor packages dependency and attribution generator (#1974)

--- build-tools/rel-build.sh
+++ build-tools/rel-build.sh
@@ -4,17 +4,8 @@ set -ex
 
 CURDIR="$(dirname $BASH_SOURCE)"
 RUN_TESTS=${RUN_TESTS:-1}
-LICENSE=${LICENSE:-0}
 
 . $CURDIR/_build-lib.sh
-
-if [ $LICENSE == 1 ]; then
-  # Licensee need this path to generate attributions
-  vendor_dir="$CURDIR/../../k8s-bigip-ctlr/vendor"
-  . $CURDIR/attributions-generator.sh
-  # Run the attributions and save the content to a local file.
-  generate_attributions_licensee $vendor_dir > /build/all_attributions.txt
-fi
 
 if [ $RUN_TESTS -eq 1 ]; then
     echo "Gathering unit test code coverage for 'release' build..."

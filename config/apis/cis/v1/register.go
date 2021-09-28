6f24359d00f311277a965ec04f8f3c78908d13c2
Sravya <46847681+sravyap135@users.noreply.github.com>
2021-09-28 14:17:39 +0530

    Policy CR type definitions and schema (#2005)

--- config/apis/cis/v1/register.go
+++ config/apis/cis/v1/register.go
@@ -46,6 +46,8 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 		&TransportServerList{},
 		&ExternalDNS{},
 		&ExternalDNSList{},
+		&Policy{},
+		&PolicyList{},
 	)
 
 	scheme.AddKnownTypes(

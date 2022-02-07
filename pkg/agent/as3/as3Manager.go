5399affa421bcef3e2b28723cd959116826487e0
Nanda Kishore Peddi <n.peddi@f5.com>
2022-02-07 20:02:54 +0530

    Stop retrying 422 Status Code

--- pkg/agent/as3/as3Manager.go
+++ pkg/agent/as3/as3Manager.go
@@ -252,7 +252,7 @@ func (am *AS3Manager) prepareTenantDeclaration(cfg *AS3Config, tenantName string
 }
 
 func (am *AS3Manager) processResponseCode(responseCode string, partition string, decl as3Declaration) {
-	if responseCode != responseStatusOk {
+	if responseCode != responseStatusOk && responseCode != responseStatusUnprocessableEntity {
 		am.failedContext.failedTenants[partition] = decl
 	} else {
 		am.excludePartitionFromFailureTenantList(partition)
@@ -290,7 +290,7 @@ func (am *AS3Manager) processTenantDeletion(tempAS3Config AS3Config) (bool, stri
 }
 
 func getResponseStatusList() map[string]int {
-	responseStatusList := map[string]int{responseStatusNotFound: 0, responseStatusServiceUnavailable: 0, responseStatusOk: 0, responseStatusCommon: 0, responseStatusDummy: 0}
+	responseStatusList := map[string]int{responseStatusNotFound: 0, responseStatusServiceUnavailable: 0, responseStatusOk: 0, responseStatusCommon: 0, responseStatusUnprocessableEntity: 0, responseStatusDummy: 0}
 	return responseStatusList
 }
 
@@ -339,6 +339,9 @@ func processResponseCodeList(responseList map[string]int) (bool, string) {
 	}
 	if responseList[responseStatusDummy] > 0 {
 		return true, responseStatusDummy
+	}
+	if responseList[responseStatusUnprocessableEntity] > 0 {
+		return true, responseStatusUnprocessableEntity
 	}
 	if responseList[responseStatusOk] > 0 {
 		return true, responseStatusOk

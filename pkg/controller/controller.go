c9cace7ec491a535c2c3e1e048d8f0f932244a53
Sravya Pondugula <sravyap135@gmail.com>
2022-02-14 16:58:02 +0530

    Agent changes for multi-partition support
    Signed-off-by: Sravya Pondugula <sravyap135@gmail.com>

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -102,6 +102,7 @@ func NewController(params Params) *Controller {
 		Agent:              params.Agent,
 		PoolMemberType:     params.PoolMemberType,
 		UseNodeInternal:    params.UseNodeInternal,
+		Partition:          params.Partition,
 		initState:          true,
 		SSLContext:         make(map[string]*v1.Secret),
 		dgPath:             strings.Join([]string{DEFAULT_PARTITION, "Shared"}, "/"),
@@ -174,9 +175,7 @@ func NewController(params Params) *Controller {
 		_ = ctlr.createIPAMResource()
 	}
 
-	respChan := make(chan int)
-	ctlr.Agent.SetResponseChannel(respChan)
-	go ctlr.responseHandler(respChan)
+	go ctlr.responseHandler(ctlr.Agent.respChan)
 	go ctlr.Start()
 	return ctlr
 }

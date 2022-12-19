a3b04a64cb94dd26f07101c27955f6c2f9fd9a6a
Vivek Lohiya <vklohiya@live.com>
2022-12-19 18:03:50 +0530

    Adding unit tests for agent package (#2679)
    Signed-off-by: Vivek Lohiya <vklohiya@live.com>
    
    Signed-off-by: Vivek Lohiya <vklohiya@live.com>

--- pkg/agent/as3/as3Manager_test.go
+++ pkg/agent/as3/as3Manager_test.go
@@ -17,10 +17,12 @@ package as3
 
 import (
 	"encoding/json"
-
+	"fmt"
 	. "github.com/F5Networks/k8s-bigip-ctlr/v2/pkg/resource"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"net/http"
+	"strings"
 )
 
 type mockAS3Manager struct {
@@ -89,11 +91,12 @@ var _ = Describe("AS3Manager Tests", func() {
 	})
 
 	Describe("Validate Generated Unified Declaration", func() {
-		It("Unified Declaration only with User Defined ConfigMaps", func() {
+		It("Unified Declaration only with User Defined ConfigMaps with filter tenant & nodeport local", func() {
 			cmcfg1 := readConfigFile(configPath + "as3config_valid_1.json")
 			cmcfg2 := readConfigFile(configPath + "as3config_valid_2.json")
-			unified := readConfigFile(configPath + "as3config_multi_cm_unified.json")
-
+			unified := readConfigFile(configPath + "as3_multi_cm_unified_nodeport_local.json")
+			mockMgr.FilterTenants = true
+			mockMgr.poolMemberType = NodePortLocal
 			mockMgr.ResourceRequest.AgentCfgmaps = append(
 				mockMgr.ResourceRequest.AgentCfgmaps,
 				&AgentCfgMap{
@@ -295,7 +298,7 @@ var _ = Describe("AS3Manager Tests", func() {
 		})
 	})
 
-	Describe("TLS Profile", func() {
+	Describe("As3 Manager functions", func() {
 		It("Default Cipher Group", func() {
 			mockMgr.enableTLS = "1.3"
 			sharedApp := as3Application{
@@ -314,6 +317,269 @@ var _ = Describe("AS3Manager Tests", func() {
 			Expect(sharedApp["virtualServer_tls_server"]).NotTo(BeNil(), "Failed to create TLS Server Profile")
 			Expect(sharedApp["virtualServer"].(*as3Service).ServerTLS).To(Equal("virtualServer_tls_server"), "Failed to set TLS Server Profile")
 			Expect(sharedApp["virtualServer_tls_server"].(*as3TLSServer).CipherGroup.BigIP).To(Equal("/Common/f5-default"), "Failed to set Default Cipher group for TLS Server Profile")
+		})
+		It("Check getTenantObjects function", func() {
+			data := mockMgr.getTenantObjects([]string{"test1", "test2"})
+			Expect(len(data)).ToNot(Equal(0))
+			Expect(strings.Contains(data, "declaration")).To(BeTrue())
+			Expect(strings.Contains(data, "class")).To(BeTrue())
+			Expect(strings.Contains(data, "Tenant")).To(BeTrue())
+			Expect(strings.Contains(data, "test1")).To(BeTrue())
+			Expect(strings.Contains(data, "test2")).To(BeTrue())
+		})
+		It("Check BigIP App services available", func() {
+			mockPM := newMockPostManger()
+			mockMgr.PostManager = mockPM.PostManager
+			mockPM.setResponses([]responceCtx{
+				{
+					tenant: "test",
+					status: http.StatusOK,
+					body:   `{"version":"3.18.1", "release":"r1", "schemaCurrent":"test"}`,
+				},
+			}, http.MethodGet)
+			Expect(mockMgr.IsBigIPAppServicesAvailable()).To(BeNil())
+			mockPM.setResponses([]responceCtx{
+				{
+					tenant: "test",
+					status: http.StatusOK,
+					body:   `{"version":"3.42.1", "release":"r1", "schemaCurrent":"test"}`,
+				},
+			}, http.MethodGet)
+			Expect(mockMgr.IsBigIPAppServicesAvailable()).To(BeNil())
+			Expect(mockMgr.as3Version).To(Equal(defaultAS3Version))
+			Expect(mockMgr.as3Release).To(Equal(defaultAS3Version + "-" + defaultAS3Build))
+			Expect(mockMgr.as3SchemaVersion).To(Equal(fmt.Sprintf("%.2f.0", as3Version)))
+			mockPM.setResponses([]responceCtx{
+				{
+					tenant: "test",
+					status: http.StatusOK,
+					body:   `{"version":"3.17.0", "release":"r1", "schemaCurrent":"test"}`,
+				},
+			}, http.MethodGet)
+			Expect(mockMgr.IsBigIPAppServicesAvailable()).ToNot(BeNil())
+		})
+	})
+	Describe("Post AS3 Declaration", func() {
+		It("Post AS3 Declaration with config deployer", func() {
+			mockMgr.l2l3Agent = L2L3Agent{
+				eventChan: make(chan interface{}, 1)}
+			mockMgr.RspChan = make(chan interface{}, 1)
+			mockMgr.ReqChan = make(chan MessageRequest, 1)
+			cfg := &ResourceConfig{
+				MetaData: MetaData{
+					Active: true,
+				},
+				Pools: []Pool{{Name: "test-pool", Partition: DEFAULT_PARTITION, ServiceName: "test-svc", ServicePort: 80, MonitorNames: []string{"test_monitor"}, Members: []Member{{Port: 80, Address: "192.168.1.1"}}}},
+			}
+			agentresources := &AgentResources{
+				RsMap:  ResourceConfigMap{},
+				RsCfgs: ResourceConfigs{},
+			}
+			agentresources.RsCfgs = append(agentresources.RsCfgs, cfg)
+			mockMgr.Resources = agentresources
+			//as3ConfigMaps := []*AS3ConfigMap{{Name: "test"}}
+			//mockMgr.as3ActiveConfig = AS3Config{configmaps: as3ConfigMaps}
+			mockPM := newMockPostManger()
+			mockPM.AS3PostDelay = 2
+			mockMgr.PostManager = mockPM.PostManager
+			go mockMgr.ConfigDeployer()
+			mockPM.setResponses([]responceCtx{{
+				tenant: "test",
+				status: http.StatusOK,
+				body:   "",
+			}}, http.MethodPost)
+			resourceRequest := ResourceRequest{
+				PoolMembers:  make(map[Member]struct{}),
+				Resources:    agentresources,
+				Profs:        map[SecretKey]CustomProfile{},
+				IrulesMap:    IRulesMap{},
+				IntDgMap:     InternalDataGroupMap{},
+				IntF5Res:     InternalF5ResourcesGroup{},
+				AgentCfgmaps: []*AgentCfgMap{},
+			}
+			Expect(mockMgr.as3ActiveConfig.resourceConfig).To(BeNil())
+			Expect(mockMgr.as3ActiveConfig.unifiedDeclaration).To(BeEmpty())
+			// putting the first request on channel
+			mockMgr.ReqChan <- MessageRequest{ReqID: 1, MsgType: "test", ResourceRequest: resourceRequest}
+			l2msg := <-mockMgr.l2l3Agent.eventChan
+			Expect(l2msg).ToNot(BeNil(), "l2l3 Channel should not be empty")
+			msg := <-mockMgr.RspChan
+			Expect(msg).ToNot(BeNil(), "response Channel should not be empty")
+			Expect(mockMgr.as3ActiveConfig.resourceConfig).ToNot(BeNil())
+			Expect(mockMgr.as3ActiveConfig.unifiedDeclaration).ToNot(BeEmpty())
+			close(mockMgr.RspChan)
+			close(mockMgr.l2l3Agent.eventChan)
+			close(mockMgr.ReqChan)
+		})
+		It("Post AS3 Declaration on event timeout", func() {
+			mockMgr.ReqChan = make(chan MessageRequest, 1)
+			agentresource := &AgentResources{
+				RsMap:  ResourceConfigMap{},
+				RsCfgs: ResourceConfigs{},
+			}
+			tnt := "test"
+			mockPM := newMockPostManger()
+			mockMgr.PostManager = mockPM.PostManager
+			mockPM.setResponses([]responceCtx{{
+				tenant: tnt,
+				status: http.StatusOK,
+				body:   "",
+			}}, http.MethodPost)
+			resourceRequest := ResourceRequest{
+				PoolMembers:  make(map[Member]struct{}),
+				Resources:    agentresource,
+				Profs:        map[SecretKey]CustomProfile{},
+				IrulesMap:    IRulesMap{},
+				IntDgMap:     InternalDataGroupMap{},
+				IntF5Res:     InternalF5ResourcesGroup{},
+				AgentCfgmaps: []*AgentCfgMap{},
+			}
+			mockMgr.ReqChan <- MessageRequest{ReqID: 1, MsgType: "test", ResourceRequest: resourceRequest}
+			result, output := mockMgr.postOnEventOrTimeout(timeoutSmall)
+			close(mockMgr.ReqChan)
+			Expect(result).To(BeTrue(), "Posting Failed")
+			Expect(output).To(Equal("statusOK"), "Posting Failed")
+		})
+		It("Validate partition deletion with tenant filtering", func() {
+			cmcfg1 := readConfigFile(configPath + "as3config_valid_1.json")
+			mockMgr.FilterTenants = true
+			mockMgr.as3ActiveConfig.tenantMap["Tenant1"] = true
+			mockMgr.as3ActiveConfig.tenantMap["Tenant2"] = true
+			mockPM := newMockPostManger()
+			mockMgr.PostManager = mockPM.PostManager
+			mockPM.setResponses([]responceCtx{{
+				tenant: "Tenant1",
+				status: http.StatusOK,
+				body:   ""},
+				{
+					tenant: "Tenant1",
+					status: http.StatusOK,
+					body:   ""},
+				{
+					tenant: "Tenant1",
+					status: http.StatusOK,
+					body:   ""},
+			}, http.MethodPost)
+			agentresource := &AgentResources{
+				RsMap:  ResourceConfigMap{},
+				RsCfgs: ResourceConfigs{},
+			}
+			resourceRequest := ResourceRequest{
+				PoolMembers: make(map[Member]struct{}),
+				Resources:   agentresource,
+				Profs:       map[SecretKey]CustomProfile{},
+				IrulesMap:   IRulesMap{},
+				IntDgMap:    InternalDataGroupMap{},
+				IntF5Res:    InternalF5ResourcesGroup{},
+			}
+			resourceRequest.AgentCfgmaps = append(
+				mockMgr.ResourceRequest.AgentCfgmaps,
+				&AgentCfgMap{
+					GetEndpoints: mockGetEndPoints,
+					Name:         "cfgmap1",
+					Namespace:    "default",
+					Data:         cmcfg1,
+					Label: map[string]string{
+						AS3Label:    TrueLabel,
+						F5TypeLabel: VSLabel,
+					},
+				},
+			)
+			mockMgr.as3ActiveConfig.unifiedDeclaration = as3Declaration(readConfigFile(configPath + "as3config_multi_cm_unified.json"))
+			result, output := mockMgr.postAS3Declaration(resourceRequest)
+			Expect(result).To(BeTrue(), "Posting Failed")
+			Expect(output).To(Equal(responseStatusOk), "Posting Failed")
+			// Verify that tenant3 is deleted from active configMap
+			_, ok := mockMgr.as3ActiveConfig.tenantMap["Tenant2"]
+			Expect(ok).To(BeFalse(), "Tenant2 Should be deleted from active configMap")
+			Expect(len(mockMgr.as3ActiveConfig.tenantMap)).To(Equal(1), "Tenant2 Should be deleted from active configMap")
+		})
+		It("Clean CIS managed Partition", func() {
+			mockPM := newMockPostManger()
+			mockMgr.PostManager = mockPM.PostManager
+			mockPM.setResponses([]responceCtx{{
+				tenant: "Tenant1",
+				status: http.StatusOK,
+				body:   ""},
+			}, http.MethodPost)
+			result, output := mockMgr.CleanAS3Tenant("Tenant1")
+			Expect(result).To(BeTrue(), "Posting Failed")
+			Expect(output).To(Equal(responseStatusOk), "Posting Failed")
+		})
+		It("Handle Post Failures", func() {
+			mockPM := newMockPostManger()
+			mockMgr.PostManager = mockPM.PostManager
+			mockPM.setResponses([]responceCtx{{
+				tenant: "Tenant1",
+				status: http.StatusOK,
+				body:   ""},
+			}, http.MethodPost)
+			mockMgr.as3ActiveConfig.unifiedDeclaration = as3Declaration(readConfigFile(configPath + "as3config_multi_cm_unified.json"))
+			result, output := mockMgr.failureHandler()
+			Expect(result).To(BeTrue(), "Posting Failed")
+			Expect(output).To(Equal(responseStatusOk), "Posting Failed")
+			mockMgr.FilterTenants = true
+			// setting failed tenant context
+			mockMgr.failedContext.failedTenants["Tenant1"] = as3Declaration("")
+			mockPM.setResponses([]responceCtx{{
+				tenant: "Tenant1",
+				status: http.StatusOK,
+				body:   ""},
+			}, http.MethodPost)
+			result, output = mockMgr.failureHandler()
+			Expect(result).To(BeTrue(), "Posting Failed")
+			Expect(output).To(Equal(responseStatusOk), "Posting Failed")
+			Expect(len(mockMgr.failedContext.failedTenants)).To(Equal(0), "Posting Failed")
+		})
+	})
+	Describe("Test processResponseCodeList", func() {
+		It("Test processResponseCodeList without any entry in Map", func() {
+			responseList := make(map[string]int)
+			result, output := processResponseCodeList(responseList)
+			Expect(result).To(BeFalse(), "Incorrect status")
+			Expect(output).To(Equal(responseStatusCommon), "Incorrect status")
+		})
+		It("Test processResponseCodeList with responseStatusServiceUnavailable", func() {
+			responseList := make(map[string]int)
+			responseList[responseStatusServiceUnavailable] = 1
+			result, output := processResponseCodeList(responseList)
+			Expect(result).To(BeFalse(), "Incorrect status")
+			Expect(output).To(Equal(responseStatusServiceUnavailable), "Incorrect status")
+		})
+		It("Test processResponseCodeList with responseStatusNotFound", func() {
+			responseList := make(map[string]int)
+			responseList[responseStatusNotFound] = 1
+			result, output := processResponseCodeList(responseList)
+			Expect(result).To(BeTrue(), "Incorrect status")
+			Expect(output).To(Equal(responseStatusNotFound), "Incorrect status")
+		})
+		It("Test processResponseCodeList with responseStatusCommon", func() {
+			responseList := make(map[string]int)
+			responseList[responseStatusCommon] = 1
+			result, output := processResponseCodeList(responseList)
+			Expect(result).To(BeFalse(), "Incorrect status")
+			Expect(output).To(Equal(responseStatusCommon), "Incorrect status")
+		})
+		It("Test processResponseCodeList with responseStatusDummy", func() {
+			responseList := make(map[string]int)
+			responseList[responseStatusDummy] = 1
+			result, output := processResponseCodeList(responseList)
+			Expect(result).To(BeTrue(), "Incorrect status")
+			Expect(output).To(Equal(responseStatusDummy), "Incorrect status")
+		})
+		It("Test processResponseCodeList with responseStatusUnprocessableEntity", func() {
+			responseList := make(map[string]int)
+			responseList[responseStatusUnprocessableEntity] = 1
+			result, output := processResponseCodeList(responseList)
+			Expect(result).To(BeTrue(), "Incorrect status")
+			Expect(output).To(Equal(responseStatusUnprocessableEntity), "Incorrect status")
+		})
+		It("Test processResponseCodeList with responseStatusOk", func() {
+			responseList := make(map[string]int)
+			responseList[responseStatusOk] = 1
+			result, output := processResponseCodeList(responseList)
+			Expect(result).To(BeTrue(), "Incorrect status")
+			Expect(output).To(Equal(responseStatusOk), "Incorrect status")
 		})
 	})
 })

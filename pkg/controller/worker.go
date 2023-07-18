74e69a53847e9afcaac760754d16befe6ac99efb
Nanda Kishore <44459460+nandakishorepeddi@users.noreply.github.com>
2023-07-18 15:25:41 +0530

    Add CRUD support for TS ExtendedServiceReferences (#2981)

--- pkg/controller/worker.go
+++ pkg/controller/worker.go
@@ -376,11 +376,18 @@ func (ctlr *Controller) processResources() bool {
 				delete(ctlr.resources.processedNativeResources, rscRefKey)
 			}
 		}
+		if rKey.event != Create {
+			// update the poolMem cache, clusterSvcResource & resource-svc maps
+			ctlr.deleteResourceExternalClusterSvcRouteReference(rscRefKey)
+		}
 		err := ctlr.processTransportServers(virtual, rscDelete)
 		if err != nil {
 			// TODO
 			utilruntime.HandleError(fmt.Errorf("Sync %v failed with %v", key, err))
 			isRetryableError = true
+		}
+		if rKey.event != Create {
+			ctlr.deleteUnrefereedMultiClusterInformers()
 		}
 	case IngressLink:
 		if ctlr.mode == OpenShiftMode || ctlr.mode == KubernetesMode {

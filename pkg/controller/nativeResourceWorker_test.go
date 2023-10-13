9cd9f50e9336f694c30dfe6e4a523e815a75fd12
Arbaaz Khan <arbaazkhan083@gmail.com>
2023-10-13 16:04:48 +0530

    Fix for local cluster health monitor name in multicluster ratio mode (#3112)

--- pkg/controller/nativeResourceWorker_test.go
+++ pkg/controller/nativeResourceWorker_test.go
@@ -2476,9 +2476,9 @@ externalClustersConfig:
 			// Verify that distinct health monitors are created for all pools in ratio mode
 			expectedHealthMonitors := make(map[string]struct{})
 			expectedHealthMonitors = map[string]struct{}{
-				"svc1_default_test_com_foo":            struct{}{},
+				"svc1_default_test_com_foo_cluster1":   struct{}{},
 				"svc1_default_test_com_foo_cluster2":   struct{}{},
-				"svc1_b_default_test_com_foo":          struct{}{},
+				"svc1_b_default_test_com_foo_cluster1": struct{}{},
 				"svc1_b_default_test_com_foo_cluster2": struct{}{},
 				"test_default_test_com_foo_cluster3":   struct{}{},
 			}

026946a6f622185ac022a556b0a8634769ab1731
Vidya Sagar <42372699+vidyasagar-m@users.noreply.github.com>
2023-12-26 14:36:37 +0530

    customise console logger with micrsoseconds in the timestamp (#3206)

--- pkg/vlogger/logger.go
+++ pkg/vlogger/logger.go
@@ -26,6 +26,17 @@ import (
 	"os"
 )
 
+const (
+	Ldate         = 1 << iota     // the date in the local time zone: 2009/01/23
+	Ltime                         // the time in the local time zone: 01:23:23
+	Lmicroseconds                 // microsecond resolution: 01:23:23.123123.  assumes Ltime.
+	Llongfile                     // full file name and line number: /a/b/c/d.go:23
+	Lshortfile                    // final file name element and line number: d.go:23. overrides Llongfile
+	LUTC                          // if Ldate or Ltime is set, use UTC rather than the local time zone
+	Lmsgprefix                    // move the "prefix" from the beginning of the line to before the message
+	LstdFlags     = Ldate | Ltime // initial values for the standard logger
+)
+
 type (
 	consoleLogger struct {
 		// slLogLevel uses syslog's definitions which have higher priority

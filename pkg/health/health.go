24a2ca82636ce92f3243828ed03c27f9538b3f98
vidyasagar-m <mvsr222@gmail.com>
2024-03-06 19:30:43 +0530

    #3279 disable default partition

--- pkg/health/health.go
+++ pkg/health/health.go
@@ -1,14 +1,39 @@
 package health
 
 import (
+	"context"
 	"net/http"
 	"os"
-
+	"k8s.io/client-go/kubernetes"
 	log "github.com/F5Networks/k8s-bigip-ctlr/v2/pkg/vlogger"
+)
+
+const (
+	clusterHealthPath = "/readyz"
 )
 
 type HealthChecker struct {
 	SubPID int
+}
+
+func (hc HealthChecker) CISHealthCheckHandler(kubeClient kubernetes.Interface) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if kubeClient != nil {
+			var response string
+			// Check if kube-api server is reachable
+			_, err := kubeClient.Discovery().RESTClient().Get().AbsPath(clusterHealthPath).DoRaw(context.TODO())
+			if err != nil {
+				response = "kube-api server is not reachable."
+			}
+			if err == nil {
+				w.WriteHeader(http.StatusOK)
+				w.Write([]byte("Ok"))
+			} else {
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte(response))
+			}
+		}
+	})
 }
 
 func (hc HealthChecker) HealthCheckHandler() http.Handler {

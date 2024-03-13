9d709fefd329680e704014f7ea24f567d0ddeb77
Arbaaz Khan <arbaazkhan083@gmail.com>
2024-03-13 09:37:09 +0530

    Add support for policy CR with serviceTypeLB

--- pkg/controller/worker.go
+++ pkg/controller/worker.go
@@ -413,16 +413,17 @@ func (ctlr *Controller) processResources() bool {
 			}
 		}
 		if ctlr.managedResources.ManageCustomResources {
-			virtuals := ctlr.getVirtualsForCustomPolicy(cp)
-			//Sync Custompolicy for Virtual Servers
-			for _, virtual := range virtuals {
-				err := ctlr.processVirtualServers(virtual, false)
-				if err != nil {
-					// TODO
-					utilruntime.HandleError(fmt.Errorf("Sync %v failed with %v", key, err))
-					isRetryableError = true
-				}
-			}
+			// TODO: Uncomment the below code once Virtual Server CR is supported for 3.x
+			//virtuals := ctlr.getVirtualsForCustomPolicy(cp)
+			////Sync Custompolicy for Virtual Servers
+			//for _, virtual := range virtuals {
+			//	err := ctlr.processVirtualServers(virtual, false)
+			//	if err != nil {
+			//		// TODO
+			//		utilruntime.HandleError(fmt.Errorf("Sync %v failed with %v", key, err))
+			//		isRetryableError = true
+			//	}
+			//}
 			//Sync Custompolicy for Transport Servers
 			tsVirtuals := ctlr.getTransportServersForCustomPolicy(cp)
 			for _, virtual := range tsVirtuals {

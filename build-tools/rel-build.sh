1931eced363b5881e0fd1be0f4c15d212ba4ab3a
mpanchal <87651292+p-maha@users.noreply.github.com>
2024-09-03 14:50:32 +0530

    3.0.0: Added debugging flags to Go build process (#4851) (#3541)
    * Add debugging flags to Go build process
    
    * Add debugging flags to Go build process

--- build-tools/rel-build.sh
+++ build-tools/rel-build.sh
@@ -3,12 +3,12 @@
 set -ex
 
 if [[ $BUILD_VERSION == "" ]]; then
-  echo "Must set BUILD_VERSION"
-  false
+    echo "Must set BUILD_VERSION"
+    false
 fi
 if [[ $BUILD_INFO == "" ]]; then
-  echo "Must set BUILD_INFO"
-  false
+    echo "Must set BUILD_INFO"
+    false
 fi
 
 go mod download
@@ -16,4 +16,4 @@ go mod download
 CGO_ENABLED=0
 GOOS=linux
 GOARCH=amd64
-go build -v -ldflags "-extldflags \"-static\" -X main.version=${BUILD_VERSION} -X main.buildInfo=${BUILD_INFO}" -o /bin/k8s-bigip-ctlr $REPOPATH/cmd/k8s-bigip-ctlr
+go build -gcflags="all=-N -l" -v -ldflags "-extldflags \"-static\" -X main.version=${BUILD_VERSION} -X main.buildInfo=${BUILD_INFO}" -o /bin/k8s-bigip-ctlr $REPOPATH/cmd/k8s-bigip-ctlr

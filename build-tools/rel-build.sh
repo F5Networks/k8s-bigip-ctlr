65522d7590ad14ddc9bd9a204776d2326887a46b
Vivek Lohiya <vklohiya@live.com>
2024-10-21 10:57:54 +0530

    fix for pipeline

--- build-tools/rel-build.sh
+++ build-tools/rel-build.sh
@@ -16,9 +16,8 @@ RUN_TESTS=${RUN_TESTS:-1}
 if [ $RUN_TESTS -eq 1 ]; then
     go install github.com/onsi/ginkgo/v2/ginkgo
     go install github.com/onsi/gomega
-	  GO111MODULE=on
-	  go get github.com/wadey/gocovmerge
-	  go get github.com/mattn/goveralls
+    go install github.com/wadey/gocovmerge@latest
+    go install github.com/mattn/goveralls@latest
     echo "Gathering unit test code coverage for 'release' build..."
     ginkgo_test_with_coverage
     # push coverage data to coveralls if F5 repo or if configured for fork.

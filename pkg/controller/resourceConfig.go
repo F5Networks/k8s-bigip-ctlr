e964d36e7c859252560ba19a505bbec42d4893ef
vidyasagar-m <mvsr222@gmail.com>
2024-10-28 17:23:47 +0530

    #3561 replace + to _ in AS3 formatted names

--- pkg/controller/resourceConfig.go
+++ pkg/controller/resourceConfig.go
@@ -2111,8 +2111,9 @@ func AS3NameFormatter(name string) string {
 		"[":  "",
 		"]":  "",
 		"=":  "_",
-		"*_": ""}
-	SpecialChars := [9]string{".", ":", "/", "%", "-", "[", "]", "=", "*_"}
+		"*_": "",
+	    "+": "_"}
+	SpecialChars := [10]string{".", ":", "/", "%", "-", "[", "]", "=", "*_", "+"}
 	for _, key := range SpecialChars {
 		name = strings.ReplaceAll(name, key, modifySpecialChars[key])
 	}

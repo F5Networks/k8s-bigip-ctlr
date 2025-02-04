e84a112385ddfc490fbbb51f43636db043c2594f
vidyasagar-m <mvsr222@gmail.com>
2025-02-04 14:34:21 +0530

    fix uts

--- pkg/controller/worker_test.go
+++ pkg/controller/worker_test.go
@@ -144,7 +144,7 @@ var _ = Describe("Worker Tests", func() {
 				options.LabelSelector = mockCtlr.multiClusterHandler.ClusterConfigs[""].nativeResourceSelector.String()
 			},
 		)
-		mockCtlr.vsAddressesMap = make(map[resourceRef]string)
+		mockCtlr.ResourceStatusVSAddressMap = make(map[resourceRef]string)
 	})
 
 	Describe("Validating Ingress link functions", func() {
@@ -3990,7 +3990,7 @@ extendedRouteSpec:
 					options.LabelSelector = mockCtlr.multiClusterHandler.ClusterConfigs[""].nativeResourceSelector.String()
 				},
 			)
-			mockCtlr.vsAddressesMap = make(map[resourceRef]string)
+			mockCtlr.ResourceStatusVSAddressMap = make(map[resourceRef]string)
 		})
 		It("Virtual Server processing on pod update", func() {
 			mockCtlr.PoolMemberType = NodePortLocal
